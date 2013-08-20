bc25cf9be0fe179017e534556063f66ba6df1ffb
Ai Jogja <syarif@jogjacamp.co.id>
2013-08-20 15:02:10 +0700

    setting smtp

--- mblayang/settings.py
+++ mblayang/settings.py
@@ -188,10 +188,10 @@ ADMIN_MEDIA_PREFIX = ""
 # Email backend to send mail
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 """
+EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'mblayang@gmail.com'
 EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 """
